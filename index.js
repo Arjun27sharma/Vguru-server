6ed214394e7058a6d740d6327f34fd1f9d3c0033
Arjun Sharma <arjunsharma6622@gmail.com>
2024-01-06 12:03:45 +0530

    added user schema, all user routes working ok

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const userRoutes = require('./routes/user')
 
 // Settings
 app.set('port', process.env.PORT || 5000);
 const DB = process.env.MONGO_URI;
 
 
+
+
 // Middlewares
 app.use(cors());
+app.use(express.json());
 app.use(express.json());
 
 // Routes
 // TODO: Add your routes here
+app.use('/api/users', userRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 // Connect to MongoDB database
 mongoose.connect(DB)
@@ -25,3 +33,5 @@ mongoose.connect(DB)
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
+
+
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['student', 'mentor', 'teacher'],
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
