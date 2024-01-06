ea99468cb9d4d0c0100a4c52168e1dcf0e34da07
Arjun Sharma <arjunsharma6622@gmail.com>
2024-01-06 12:21:44 +0530

    cors issue resolved

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ const DB = process.env.MONGO_URI;
 
 
 
+// CORS options
+const corsOptions = {
+    origin: 'http://localhost:5173', // Only allow requests from this origin
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true, // Allow sending cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+};
 
 // Middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.json());
 
